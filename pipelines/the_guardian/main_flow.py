fdf2473beb2823acc3123afe94d97dd68ff573bf
Carlos Munoz <carlos7ags@gmail.com>
2022-12-27 22:30:30 -0600

    add main flow

--- pipelines/the_guardian/main_flow.py
+++ pipelines/the_guardian/main_flow.py
@@ -1,12 +1,17 @@
 import os
 
 from prefect import flow
-from .data_extractor import extract_data_flow
+
+from pipelines.the_guardian.data_extractor import extract_data_flow
+from pipelines.the_guardian.data_loader import load_data_flow
+from pipelines.the_guardian.data_processor import process_data_flow
 
 
 @flow
 def main_flow() -> None:
     raw_data = extract_data_flow()
+    processed_data = process_data_flow(raw_data)
+    load_data_flow(processed_data)
 
 
 def run():
